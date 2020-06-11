cc345c9d37f8509f88ae213cb41feb79e4e6c161
dreamacheng <achengiad@qq.com>
2020-06-11 18:23:09 +0800

    1. 单调栈思想

---
+++ src/exm/algo/array/DailyTemperatures.java
@@ -0,0 +1,41 @@
+package exm.algo.array;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class DailyTemperatures {
+
+    public static void main(String[] args) {
+        int[] temperatures = {73, 74, 75, 71, 69, 72, 76, 73};
+        System.out.println(Arrays.toString(dailyTemperatures(temperatures)));
+    }
+
+    /**
+     * 请根据每日 气温 列表，重新生成一个列表。对应位置的输出为：要想观测到更高的气温，至少需要等待的天数。如果气温在这之后都不会升高，请在该位置用 0 来代替。
+     *
+     * 例如，给定一个列表 temperatures = [73, 74, 75, 71, 69, 72, 76, 73]，你的输出应该是 [1, 1, 4, 2, 1, 1, 0, 0]。
+     *
+     * 提示：气温 列表长度的范围是 [1, 30000]。每个气温的值的均为华氏度，都是在 [30, 100] 范围内的整数。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/daily-temperatures
+     * @param T
+     * @return
+     */
+    public static int[] dailyTemperatures(int[] T) {
+        if (T == null || T.length < 1) {
+            return null;
+        }
+        int len = T.length;
+        int[] res = new int[len];
+        // 使用单调栈，栈空或温度递减时直接入栈
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < len; i++) {
+            while (!stack.isEmpty() && T[i] > T[stack.peek()]) {
+                res[stack.peek()] = i - stack.pop();
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+}
